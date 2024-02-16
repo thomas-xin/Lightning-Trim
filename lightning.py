f3f1fe22f83e570b4cc0140ce4752bff4b1eb5df
Thomas <thomasxin@gmail.com>
2024-02-16 16:20:56 +1100

    vastly improved seeking efficiency

--- lightning.py
+++ lightning.py
@@ -49,7 +49,11 @@ if len(sys.argv) >= 4:
 	easygui = None
 else:
 	import easygui
-	fi = easygui.fileopenbox("Please select file to trim:")
+	fi = None
+	if len(sys.argv) >= 2:
+		fi = sys.argv[1]
+	if not fi:
+		fi = easygui.fileopenbox("Please select file to trim:")
 	if not fi:
 		raise SystemExit
 	dur = subprocess.check_output(["ffprobe", "-skip_frame", "nokey", "-select_streams", "v:0", "-show_entries", "format=duration", "-of", "default=nokey=1:noprint_wrappers=1", "-i", fi]).strip().decode("ascii")
@@ -79,7 +83,7 @@ else:
 		easygui.exceptionbox()
 
 try:
-	proc = subprocess.Popen(["ffprobe", "-skip_frame", "nokey", "-select_streams", "v:0", "-show_entries", "frame=pts_time", "-of", "default=nokey=1:noprint_wrappers=1", "-i", fi], stdout=subprocess.PIPE)
+	proc = subprocess.Popen(["ffprobe", "-read_intervals", str(max(0, start - 30)) + "%+60", "-skip_frame", "nokey", "-select_streams", "v:0", "-show_entries", "frame=pts_time", "-of", "default=nokey=1:noprint_wrappers=1", "-i", fi], stdout=subprocess.PIPE)
 
 	key = 0
 	while key < start:
@@ -89,20 +93,23 @@ try:
 	if os.path.exists(fn):
 		os.remove(fn)
 
-	if not start:
-		subprocess.run(["ffmpeg", "-y", "-hwaccel", "auto", "-ss", str(start), "-to", str(end), "-i", fi, "-c", "copy", fn])
-	elif abs(start - key) < 1 / 30:
-		subprocess.run(["ffmpeg", "-y", "-hwaccel", "auto", "-i", fi, "-ss", str(start), "-to", str(end), "-c", "copy", fn])
+	if abs(start - key) < 1 / 30:
+		subprocess.run(["ffmpeg", "-y", "-hwaccel", "auto", "-ss", str(start // 1), "-to", str(end), "-i", fi, "-ss", str(start % 1), "-c", "copy", fn])
 	else:
 		fa = name + "~0" + "." + "ts"
-		aproc = subprocess.Popen(["ffmpeg", "-y", "-hwaccel", "auto", "-i", fi, "-ss", str(start), "-to", str(end), "-vn", "-c:a", "copy", fa])
+		aproc = subprocess.Popen(["ffmpeg", "-y", "-hwaccel", "auto", "-ss", str(start // 1), "-to", str(end), "-i", fi, "-ss", str(start % 1), "-vn", "-c:a", "copy", fa])
 
 		f1 = name + "~1" + "." + "ts"
 		f2 = name + "~2" + "." + "ts"
 		cdc = subprocess.check_output(["ffprobe", "-skip_frame", "nokey", "-select_streams", "v:0", "-show_entries", "stream=codec_name", "-of", "default=nokey=1:noprint_wrappers=1", "-i", fi]).strip()
 
-		vproc = subprocess.Popen(["ffmpeg", "-y", "-hwaccel", "auto", "-i", fi, "-ss", str(start), "-to", str(key), "-crf", "20", "-c:v", cdc, "-an", f1])
-		subprocess.run(["ffmpeg", "-y", "-hwaccel", "auto", "-ss", str(key + 0.001), "-to", str(end), "-i", fi, "-c:v", "copy", "-an", "-avoid_negative_ts", "1", f2])
+		vproc = subprocess.Popen(["ffmpeg", "-y", "-hwaccel", "auto", "-ss", str(start), "-to", str(key), "-i", fi, "-crf", "20", "-c:v", cdc, "-an", f1])
+		fps = subprocess.check_output(["ffprobe", "-skip_frame", "nokey", "-select_streams", "v:0", "-show_entries", "stream=avg_frame_rate", "-of", "default=nokey=1:noprint_wrappers=1", "-i", fi]).strip().decode("ascii")
+		if not fps or fps == "N/A":
+			fps = 1000
+		else:
+			fps = eval(fps, {}, {})
+		subprocess.run(["ffmpeg", "-y", "-hwaccel", "auto", "-ss", str(key + 1 / fps), "-to", str(end), "-i", fi, "-c:v", "copy", "-an", "-avoid_negative_ts", "1", f2])
 		aproc.wait()
 		vproc.wait()
 
@@ -111,8 +118,6 @@ try:
 			f.write("file " + repr(f1) + "\n")
 			f.write("file " + repr(f2) + "\n")
 		subprocess.run(["ffmpeg", "-y", "-hwaccel", "auto", "-safe", "0", "-f", "concat", "-i", fc, "-i", fa, "-c:v", "copy", "-c:a", "copy", fn])
-		if not os.path.exists(fn):
-			raise RuntimeError("Unable to save file. Please check log for info.")
 		for fd in (fa, f1, f2, fc):
 			try:
 				os.remove(fd)
